ce1df9b77e322479fe50c61bab5d7ad85d996e79
jayDevariya2027 <jaydevariya2027@gmail.com>
2025-06-21 22:19:21 +0530

    add base_url in env

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", 
+  baseURL: BASE_URL, 
 });
 
 export default api;
